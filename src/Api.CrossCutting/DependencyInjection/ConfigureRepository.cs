9b7d7f104b1998a0649a8baae218d749a055ebad
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-08-24 23:19:54 -0300

    Adding Dependency Injection.

--- src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
+++ src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
@@ -1,15 +1,22 @@
 using Api.Data.Context;
+using Api.Data.Implementations;
 using Api.Data.Repository;
 using Api.Domain.Interfaces;
+using Api.Domain.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Api.CrossCutting.DependencyInjection {
-    public class ConfigureRepository {
-        public static void ConfigureDependenciesService (IServiceCollection serviceCollection) {
-            serviceCollection.AddScoped (typeof (IRepository<>), typeof (BaseRepository<>));
-            serviceCollection.AddDbContext<MyContext> (
-                opt => opt.UseSqlServer ("Data Source=DESKTOP-D9VV8QN;Initial Catalog=dbAPI;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"));
+namespace Api.CrossCutting.DependencyInjection
+{
+    public class ConfigureRepository
+    {
+        public static void ConfigureDependenciesService(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+            serviceCollection.AddScoped<IUserRepository, UserImplementation>();
+
+            serviceCollection.AddDbContext<MyContext>(
+                opt => opt.UseSqlServer("Data Source=DESKTOP-D9VV8QN;Initial Catalog=dbAPI;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"));
         }
     }
 }
--- src/Api.CrossCutting/DependencyInjection/ConfigureService.cs
+++ src/Api.CrossCutting/DependencyInjection/ConfigureService.cs
@@ -2,10 +2,14 @@ using Api.Domain.Interfaces.Services.User;
 using Api.Service.Services;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Api.CrossCutting.DependencyInjection {
-    public class ConfigureService {
-        public static void ConfigureDependenciesService (IServiceCollection serviceCollection) {
-            serviceCollection.AddTransient<IUserService, UserService> ();
+namespace Api.CrossCutting.DependencyInjection
+{
+    public class ConfigureService
+    {
+        public static void ConfigureDependenciesService(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<IUserService, UserService>();
+            serviceCollection.AddTransient<ILoginService, LoginService>();
         }
     }
 }
